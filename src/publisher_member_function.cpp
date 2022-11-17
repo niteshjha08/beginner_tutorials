debbc60defafbaed711d9bc19816e99b6a1f6dc7
Nitesh Jha <niteshj818@gmail.com>
2022-11-16 23:45:50 -0500

    added license comments to cpp files

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -1,4 +1,28 @@
 /**
+MIT License
+
+Copyright (c) 2022 Nitesh Jha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
  * @file publisher_member_function.cpp
  * @author Nitesh Jha (niteshj@umd.edu)
  * @brief
@@ -25,9 +49,15 @@ using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node {
  public:
+ /**
+  * @brief Constructor for ROS2 node
+  * 
+  */
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+    // Create publisher
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
 
+    // get launch arg for start of count
     auto param_desc = rcl_interfaces::msg::ParameterDescriptor{};
     param_desc.description = "Count starts from this parameter value";
 
@@ -38,10 +68,12 @@ class MinimalPublisher : public rclcpp::Node {
     this->get_parameter("count").get_parameter_value().get<int>();
     count_ = count;
     RCLCPP_DEBUG_STREAM(this->get_logger(), "Count begins from: " << count_);
+    // timer for publishing at regular intervals
     timer_ = this->create_wall_timer(
         500ms, std::bind(&MinimalPublisher::timer_callback, this));
     pub_message = "Fear the turtle!";
     RCLCPP_DEBUG_STREAM(this->get_logger(), "Initial string: " << pub_message);
+    // create callback for service
     modify_string_service_ =
         this->create_service<cpp_pubsub::srv::ModifyString>(
             "/modify_string",
@@ -51,6 +83,12 @@ class MinimalPublisher : public rclcpp::Node {
   }
 
  private:
+ /**
+  * @brief Service callback function which modifies the published string
+  * 
+  * @param request string
+  * @param response string
+  */
   void modify(
       const std::shared_ptr<cpp_pubsub::srv::ModifyString::Request> request,
       std::shared_ptr<cpp_pubsub::srv::ModifyString::Response> response) {
@@ -59,7 +97,7 @@ class MinimalPublisher : public rclcpp::Node {
     RCLCPP_WARN_STREAM(this->get_logger(),
                        "Modified string to: " << pub_message);
   }
-
+  // publish string at regular intervals 
   void timer_callback() {
     auto message = std_msgs::msg::String();
     message.data = pub_message + std::to_string(count_++);
--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -1,4 +1,28 @@
 /**
+MIT License
+
+Copyright (c) 2022 Nitesh Jha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/**
  * @file subscriber_member_function.cpp
  * @author Nitesh Jha (niteshj@umd.edu)
  * @brief
@@ -16,14 +40,27 @@
 
 using std::placeholders::_1;
 
+/**
+ * @brief Class for ROS2 node 
+ * 
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
+ /**
+  * @brief Constructor for node
+  * 
+  */
   MinimalSubscriber() : Node("minimal_subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
  private:
+ /**
+  * @brief callback for subscriber
+  * 
+  * @param msg 
+  */
   void topic_callback(const std_msgs::msg::String& msg) const {
     RCLCPP_FATAL(this->get_logger(), "I heard: '%s'", msg.data.c_str());
   }
